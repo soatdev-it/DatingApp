856b1f5ff8b20469b16fd8b6bd3f71798fb2eb73
Stephane Thiam <stef@immowebexpert.ca>
2019-07-21 19:12:13 -0400

    Improved UI and added register component

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+import { registerModuleFactory } from '@angular/core/src/linker/ng_module_factory_loader';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  @Output() cancelRegister = new EventEmitter();
+
+  model: any = {};
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(
+      () => {
+        console.log('Registration sucessfull');
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('Canceled');
+  }
+
+}
