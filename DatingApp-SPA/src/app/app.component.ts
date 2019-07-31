43cf0042b99d472a494cce1c04c5d69a39929fbd
Stephane Thiam <stef@immowebexpert.ca>
2019-07-30 20:54:12 -0400

    Added routing and guards

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './_services/auth.service';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
 
   constructor(private authService: AuthService ) {}
 
-  ngOnInit(){
+  ngOnInit() {
     const token = localStorage.getItem('token');
     if (token) {
       this.authService.decodedToken = this.jwtHelper.decodeToken(token);
